aee7940acc7635b3a964f60f72b500fe609d4df5
poorna162005 <166363155+poorna162005@users.noreply.github.com>
2024-07-14 11:35:14 +0530

    1

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+$username = $_POST["username"];
+$email = $_POST["email"];
+echo "The username is :". $username . "<br>" ;
+echo "email is :" . $email;
+?>
---
+++ php1.php
@@ -0,0 +1,7 @@
+<?php
+//echo "Welcome to PHP"
+$num1 = 5;
+$num2 = 10;
+$result = $num1 * $num2;
+echo  "The result is ".$result;
+?>
